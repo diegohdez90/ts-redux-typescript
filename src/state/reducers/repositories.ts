4a5dfa5280357f6353a6542c24a4e92552607135
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-12-19 10:01:48 -0600

    feat: add repos reducer

---
+++ src/state/reducers/repositories.ts
@@ -0,0 +1,34 @@
+import { REPOSITORIES_ACTIONS } from "../../utils/constants";
+
+interface Repositories {
+    loading: boolean;
+    error: string | null,
+    data: string[]
+}
+
+const reducer = (state: Repositories, action: any): Repositories => {
+  switch (action.type) {
+    case REPOSITORIES_ACTIONS.SEARCH_REPOSITORIES:
+      return {
+        loading: true,
+        error: null,
+        data: []
+      }
+    case REPOSITORIES_ACTIONS.SEARCH_REPOSITORIES_ERROR:
+      return {
+        loading: false,
+        error: action.payload,
+        data: []
+      }
+    case REPOSITORIES_ACTIONS.SEARCH_REPOSITORIES_SUCCESS:
+      return {
+        loading: false,
+        error: null,
+        data: action.payload
+      }
+    default:
+      return state;
+  }
+}
+
+export default reducer;
