c044c9600334a70542ef651f56025bdf260ea5bb
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-12-21 11:59:19 -0600

    feat: add repos components

---
+++ src/components/Repos/Form.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+      <form>
+        <label>
+          Type repo name
+          <input />
+        </label>
+      </form>
+    </div>
+  )
+}
+
+export default Form
